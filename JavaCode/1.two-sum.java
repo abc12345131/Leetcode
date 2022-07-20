5b57dcaca460cd2749b93bc0e7f358dc52c4f4b2
BaolongWu <99288473+BaolongWu@users.noreply.github.com>
2022-07-20 16:16:38 -0400

    review

--- JavaCode/1.two-sum.java
+++ JavaCode/1.two-sum.java
@@ -1,13 +1,24 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+import java.util.HashMap;
+import java.util.Map;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
+            int j = target - nums[i]; 
+            if(map.containsKey(j)) {
+                return new int[] { map.get(j), i };
             }
             map.put(nums[i], i);
         }
         throw new IllegalArgumentException("No two sum solution");
     }
 }
+// @lc code=end
+
--- JavaCode/9.palindrome-number.java
+++ JavaCode/9.palindrome-number.java
@@ -1,3 +1,10 @@
+/*
+ * @lc app=leetcode id=9 lang=java
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
 class Solution {
     public boolean isPalindrome(int x) {
         if  (x<0){
@@ -13,3 +20,5 @@ class Solution {
         }
     }  
 }
+// @lc code=end
+
